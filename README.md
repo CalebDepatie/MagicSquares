2385a0615becebacf89c5f57ab30d414f5754ce8
CalebDepatie <caleb.depatie@outlook.com>
2021-03-16 21:18:32 -0400

    Mostly Working
    Minor bugs and singly even order magic squares needs to be implemented

--- README.md
+++ README.md
@@ -1,46 +1,7 @@
-# Getting Started with Create React App
+This projet creates and displays [magic squares](https://en.wikipedia.org/wiki/Magic_square) to the size specified by the user.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This was a simple project to enhance my knowledge of both typescript and React.
 
-## Available Scripts
+I used the algorithms described in [this paper](https://www.qcc.cuny.edu/engtech/ET_Faculty/Brumgnach/Magic_Squares/Gamma_plus_two_method_for_odd_magic_squares.pdf) for determining the magic squares.
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+There are various irritating bugs with this such as the array not being initially created as a magic square and large numbers being unreadable in the matrix, these will be fixed in time. Similarly, this code presently can only handle 2 of the three cases, odd order and doubly even order magic squares.
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,215 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Square(props: any) {
+  return (
+    <div className="square">
+      {props.value}
+    </div>
+  );
+}
+
+class Board extends React.Component<{size: number}, {size:number, squares: Array<number>}> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      size: props.size,
+      squares: Array(props.size*props.size).fill(0)
+    }
+
+    this.setState({size: props.size}, this.makeMagic) // ensure it only attempts to create the magic square after state goes through
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps: any) {
+    this.setState({
+      size: nextProps.size,
+      squares: Array(nextProps.size*nextProps.size).fill(0)
+    }, this.makeMagic);
+  }
+
+  // get the index with respect to the current 4x4 square (For Durer method)
+  durerNormToSeg(i: number, seg_len: number): number {
+    if (i < seg_len) {
+      return i;
+    } else {
+      return this.durerNormToSeg(i-seg_len, seg_len);
+    }
+  }
+
+  makeMagic() {
+    // compute magic squares array
+
+    var curNum: number = 1
+    var arr            = Array(this.state.size*this.state.size).fill(0)
+
+    // eslint-disable-next-line
+    if ((this.state.size % 2 == 0) && (this.state.size % 4 == 0)) { // doubly even
+      // Durer method
+      let i     = 0 // row
+      let j     = 0 // col
+      const N   = Math.pow(this.state.size / 4, 2); // number of 4x4 squares
+      const G   = this.state.size / 4; // number of segments of an individual row or column
+      const SEG = this.state.size / G; // the length of each segment
+      for (let _i = 0; _i < this.state.size; _i++) {
+        for (let _j = 0; _j < this.state.size; _j++) {
+          let index = j + this.state.size*i
+
+          let i_norm = this.durerNormToSeg(i, SEG);
+          let j_norm = this.durerNormToSeg(j, SEG);
+
+          //console.log(curNum + ": " + i_norm)
+
+          if (i_norm == 0 || i_norm == 3) { // outer
+            if (j_norm == 0 || j_norm == 3) {
+              arr[index] = curNum;
+            }
+          } else { // inner
+            if (j_norm == 1 || j_norm == 2) {
+              arr[index] = curNum;
+            }
+          }
+
+          curNum++;
+          j += 1;
+        }
+        j  = 0;
+        i += 1;
+      }
+      // fill in the remaining places moving backwards
+      curNum = 1;
+      j = this.state.size-1;
+      i = this.state.size-1;
+      for (let _i = 0; _i < this.state.size; _i++) {
+        for (let _j = 0; _j < this.state.size; _j++) {
+          let index = j + this.state.size*i
+
+          if (arr[index] == 0) {
+            arr[index] = curNum;
+          }
+
+          curNum++;
+          j -= 1;
+        }
+        j  = this.state.size-1;
+        i -= 1;
+      }
+
+    // eslint-disable-next-line
+    } else if ((this.state.size % 2 == 0) && (this.state.size % 4 != 0)) { // singly even
+      // Brumgnach-Strachey method
+
+
+
+    } else { // odd
+      // gamma plus two
+      // start one right of the centre
+      var i = Math.floor(this.state.size /2) // row
+      var j = Math.floor(this.state.size /2) + 1// col
+      for (let num = 0; num < this.state.size; num++) {
+        for (let m = 0; m < this.state.size; m++) {
+          let index = j + this.state.size*i
+          //console.log(curNum + ": " + i + ", " + j)
+          arr[index] = curNum++
+
+          --i; ++j;
+          if (i < 0) {
+            i = this.state.size-1
+          }
+          if (j >= this.state.size) {
+            j = 0
+          }
+        }
+        // each time it completes M numbers, move 2 cells right
+        j += 1
+        ++i // revert i
+        if (i >= this.state.size) {
+          i = 0
+        }
+        if (j >= this.state.size) {
+          j = 0 + (j - this.state.size)
+        }
+      }
+    }
+
+    /*for (let i = 0; i < this.state.size; i++) {
+      for (let j = 0; j < this.state.size; j++) {
+        let index = j + this.state.size*i
+
+      }
+    } */
+
+    this.setState({
+      squares: arr
+    }, this.render)
+  }
+
+  renderSquare(i: number) {
+    //console.log(i)
+    return <Square
+            value={i}
+            />;
+  }
+
+  render() {
+    let content: Array<any> = []
+
+    content.push(<div>{"Magic Sum:" + ((this.state.size*(this.state.size*this.state.size+1))/2) } </div>)
+
+    for (let i = 0; i < this.state.size; i++) {
+      let row: Array<any> = []
+      for (let j = 0; j < this.state.size; j++) {
+        row.push(<div>{this.renderSquare(this.state.squares[j + this.state.size*i])}</div>)
+      }
+      content.push(<div className="row">{row}</div>)
+    }
+
+    return (
+      <div>
+          {content}
+      </div>
+    );
+  }
+}
+
+class Manager extends React.Component<{}, {size: number}> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      size: 4,// default magic square size
+    }
+    this.setState({size: 4}, this.render)
+  }
+
+  handleChange(evt: any) {
+    const size = (evt.target.validity.valid) ? evt.target.value : this.state.size;
+    if (size < 3) {
+      alert("Size must be atleast 3")
+    } else {
+      this.setState({size}, this.render);
+    }
+    //console.log(size)
+  }
+
+  render() {
+    return (
+      <div className="manager">
+      {"Enter size of magic square: "}
+      <input type="tel" pattern="[0-9]*" onInput={this.handleChange.bind(this)} value={this.state.size} />
+        <div className="magic-board">
+          <Board
+            size={this.state.size}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Manager />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
